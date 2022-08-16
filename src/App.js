dd3f940ff39605071f4f4f3e1dae29aa198d55e9
Aleksandr Kobelskiy <kobel.san.sanich@gmail.com>
2022-08-16 17:23:58 +0600

    Added subjx Library

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import subjx from 'subjx';
+import 'subjx/dist/style/subjx.css';
+
+const myInstance = subjx('.test-resize-block').drag({
+	// parent container
+	container: '.container',
+	// "controls" append to this element
+	controlsContainer: '.test-resize-block',
+	// 'x' | 'y' | 'xy'
+	axis: 'x',
+	// snap to grid
+	snap: {
+		x: 10,
+		y: 10,
+		angle: 10
+	},
+	// mimic behavior with other '.draggable' elements
+	each: {
+		move: false,
+		resize: false,
+		rotate: false
+	},
+	// enable/disable actions
+	draggable: true,
+	resizable: true,
+	rotatable: true,
+	scalable: false,
+	// preserve aspect ratio when resizing
+	proportions: true,
+	// restrict element dragging/resizing/rotation
+	restrict: '.selector',
+	//  'n' | 's' | 'w' | 'e'
+	rotatorAnchor: 'e',
+	// rotate offset
+	rotatorOffset: 50,
+});
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="header">
+
+			</header>
+			<div className="container">
+				{/*<div draggable="true" className="test-resize-block"></div>*/}
+				{/*<div draggable="true" className="test-resize-block"></div>*/}
+				{/*<div draggable="true" className="test-resize-block"></div>*/}
+				<div className="test-resize-block"></div>
+				{/*<div className="test-resize-block"></div>*/}
+				{/*<div className="test-resize-block"></div>*/}
+			</div>
+			<footer className="footer">
+
+			</footer>
+		</div>
+	);
 }
 
 export default App;
