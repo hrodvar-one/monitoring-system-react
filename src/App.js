72df8dee5acd8456b257282cdd6ca99db4dc020f
Aleksandr Kobelskiy <kobel.san.sanich@gmail.com>
2022-08-29 11:06:26 +0600

    Added typescript to the current project

--- src/App.js
+++ src/App.js
@@ -1,57 +1,17 @@
 import './App.scss';
-import subjx from 'subjx';
-import 'subjx/dist/style/subjx.css';
-
-const myInstance = subjx('.test-resize-block').drag({
-	// parent container
-	container: '.container',
-	// "controls" append to this element
-	controlsContainer: '.test-resize-block',
-	// 'x' | 'y' | 'xy'
-	axis: 'x',
-	// snap to grid
-	snap: {
-		x: 10,
-		y: 10,
-		angle: 10
-	},
-	// mimic behavior with other '.draggable' elements
-	each: {
-		move: false,
-		resize: false,
-		rotate: false
-	},
-	// enable/disable actions
-	draggable: true,
-	resizable: true,
-	rotatable: true,
-	scalable: false,
-	// preserve aspect ratio when resizing
-	proportions: true,
-	// restrict element dragging/resizing/rotation
-	restrict: '.selector',
-	//  'n' | 's' | 'w' | 'e'
-	rotatorAnchor: 'e',
-	// rotate offset
-	rotatorOffset: 50,
-});
+// import subjx from 'subjx';
+// import 'subjx/dist/style/subjx.css';
 
 function App() {
 	return (
 		<div className="App">
-			<header className="header">
-
-			</header>
+			<header className="header"></header>
 			<div className="container">
-				{/*<div draggable="true" className="test-resize-block"></div>*/}
-				{/*<div draggable="true" className="test-resize-block"></div>*/}
-				{/*<div draggable="true" className="test-resize-block"></div>*/}
-				<div className="test-resize-block"></div>
-				{/*<div className="test-resize-block"></div>*/}
-				{/*<div className="test-resize-block"></div>*/}
+				<div className="test">
+					test
+				</div>
 			</div>
 			<footer className="footer">
-
 			</footer>
 		</div>
 	);
